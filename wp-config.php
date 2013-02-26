3e8f2ebe807cbd5e2d03c7661bc420f215f3dfdf
Matthew Gerring <beatpanda@gmail.com>
2013-02-25 19:25:47 -0800

    Update wp-config.php
    in case you're old school and use MAMP without editing the vhosts file. Barbaric? Absolutely!

--- wp-config.php
+++ wp-config.php
@@ -16,8 +16,8 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 // ========================
 // Custom Content Directory
 // ========================
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+if(!defined('WP_CONTENT_DIR')) define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+if(!defined('WP_CONTENT_URL')) define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // ================================================
 // You almost certainly do not want to change these
